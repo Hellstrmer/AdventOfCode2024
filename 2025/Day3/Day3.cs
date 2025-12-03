4e3ef478e773e99f9581196a5be2b273f452a7df
JesperHellstrom <jesper.hellstrom@afry.com>
2025-12-03 20:30:01 +0100

    Day3 Part2

--- 2025/Day3/Day3.cs
+++ 2025/Day3/Day3.cs
@@ -6,36 +6,8 @@ namespace AdventOfCode._2025
         string Example = "Example.txt";
         string Input = "Input.txt";
         bool example = false;
+        int foundPos = 0;
         public void FirstStar()
-        {
-            List<String> Inputs = example ? helper.ReadFileList(Example) : helper.ReadFileList(Input);            
-            int res = 0;
-
-            foreach(var s in Inputs)
-            {
-                int Biggest = 0;
-                int BiggestPos = 0;
-                int secondBiggest = 0;
-                for (int i = 0; i < s.Length; i++)
-                {
-                    int n = Int32.Parse(s[i].ToString());
-                    if (n > Biggest && i < s.Length -1)
-                    {
-                        Biggest = n;
-                        BiggestPos = i;
-                    }
-                }
-                for (int i = BiggestPos + 1; i < s.Length; i++)
-                {
-                    int n = Int32.Parse(s[i].ToString());
-                    if (n > secondBiggest)                    
-                        secondBiggest = n;                    
-                }                
-                res += Int32.Parse(Biggest.ToString() + secondBiggest.ToString());
-            }
-            Console.WriteLine(res);
-        }
-        public void SecondStar()
         {
             List<String> Inputs = example ? helper.ReadFileList(Example) : helper.ReadFileList(Input);
             int res = 0;
@@ -45,19 +17,77 @@ namespace AdventOfCode._2025
                 int Biggest = 0;
                 int BiggestPos = 0;
                 int secondBiggest = 0;
-                var t = s.ToList();
-                var resList = new List<int>();
-
-                for(int i = 0; i < t.Count; i++) {
+                for (int i = 0; i < s.Length; i++)
+                {
+                    int n = Int32.Parse(s[i].ToString());
+                    if (n > Biggest && i < s.Length - 1)
                     {
-                        if (resList.Count <= 12)
-                        resList.Add(t.Max());
+                        Biggest = n;
+                        BiggestPos = i;
                     }
-                Console.WriteLine(t);
-                
+                }
+                for (int i = BiggestPos + 1; i < s.Length; i++)
+                {
+                    int n = Int32.Parse(s[i].ToString());
+                    if (n > secondBiggest)
+                        secondBiggest = n;
+                }
                 res += Int32.Parse(Biggest.ToString() + secondBiggest.ToString());
             }
             Console.WriteLine(res);
+        }
+        public void SecondStar()
+        {
+            List<String> Inputs = example ? helper.ReadFileList(Example) : helper.ReadFileList(Input);
+            ulong res = 0;
+
+            foreach (var s in Inputs)
+            {
+                ulong Biggest = 0;
+                List<ulong> Total = new List<ulong>();
+
+                ulong secondBiggest = 0;
+                var t = s.ToList();
+                var resList = new List<ulong>();
+                int BiggestPos = t.Count - 11;
+                
+                int start = 0;
+
+                while (Total.Count < 12)
+                {
+                    ulong resNumb = findBiggest(start, BiggestPos, s);
+                    Total.Add(resNumb);
+                    start = foundPos + 1;
+                    BiggestPos++;
+                }
+                string resString = "";
+                foreach (var str in Total)
+                {
+                    resString += str.ToString();
+                }
+                res += ulong.Parse(resString);
+            }
+            Console.WriteLine(res);
+        }
+
+        ulong findBiggest(int start, int end, string s)
+        {
+            ulong Biggest = 0;
+
+            for (int i = start; i < end; i++)
+            {
+                {
+                    ulong n = ulong.Parse(s[i].ToString());
+                    //Console.WriteLine("Numb: " + n);
+                    if (n > Biggest && i < s.Length)
+                    {
+                        Biggest = n;
+                        foundPos = i;
+                    }
+                }
+            }
+
+            return Biggest;
         }
     }
 }
