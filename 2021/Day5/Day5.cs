e776e76b62cc75a91cb6f629425e95eeb5715b78
jesper.Helsltrom <jesper.hellstrm@gmail.com>
2025-12-23 20:48:09 +0100

    2021 Day5 Part1 started

---
+++ 2021/Day5/Day5.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode.Helpers;
+using System.Drawing;
+namespace AdventOfCode._2021
+{
+    internal class Day5() : HelperClass
+    {
+        string Example = "Example.txt";
+        string Input = "Input.txt";
+        bool example = true;
+        public void FirstStar()
+        {
+            List<string> Inputs = example ? ReadFileList(Example) : ReadFileList(Input);
+
+            List<(int X1, int Y1, int X2, int Y2)> inp = Inputs
+                .Select(s => s.Replace(">", "")
+                .Split(',', '-'))
+                .Select(Coordinates => (X1: Int32.Parse(Coordinates[0]), Y1: Int32.Parse(Coordinates[1]), X2: Int32.Parse(Coordinates[2]), Y2: Int32.Parse(Coordinates[3])))
+                .ToList();
+
+            Console.Write(inp[0].X1);
+        }
+        public void SecondStar()
+        {
+
+        }
+    }
+}
+
+       
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using AdventOfCode._2021;
 using AdventOfCode.Helpers;
 HelperClass helper = new HelperClass();
-Day4 Day = new Day4();
+Day5 Day = new Day5();
 
 Console.WriteLine("Part1: ");
 Day.FirstStar();
