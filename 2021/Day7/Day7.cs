b9c6c8c6b5f56bb2919785a2dd2ad6f02b047348
jesper.Helsltrom <jesper.hellstrm@gmail.com>
2025-12-27 13:16:43 +0100

    2021 Day7

---
+++ 2021/Day7/Day7.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCode.Helpers;
+namespace AdventOfCode._2021
+{
+    internal class Day7() : HelperClass
+    {
+        string Example = "Example.txt";
+        string Input = "Input.txt";
+        bool example = false;
+        public void FirstStar()
+        {
+            string Inputs = example ? ReadFileString(Example) : ReadFileString(Input);
+            var Crabs = Inputs.Split(',')       
+                .Select(int.Parse)
+                .ToList();
+            int Res = 0;
+            int Position = 1;
+            while (Position < Crabs.Max())
+            {
+                int Distance = 0;
+                for (int i = 0; i < Crabs.Count; i++)                
+                    Distance += Math.Abs(Crabs[i] - Position);                
+                if (Distance < Res|| Position == 1)                
+                    Res = Distance;
+                Position++;
+            }
+            Console.WriteLine(Res);
+        }
+        public void SecondStar()
+        {
+            string Inputs = example ? ReadFileString(Example) : ReadFileString(Input);
+            var Crabs = Inputs.Split(',')
+                .Select(int.Parse)
+                .ToList();
+            int Res = 0;
+            int Position = 1;
+            while (Position < Crabs.Max())
+            {
+                int Distance = 0;
+                for (int i = 0; i < Crabs.Count; i++)
+                {
+                    if (Crabs[i] > Position)
+                    {
+                        for (int j = Crabs[i]; j > Position; j--)                        
+                            Distance += Math.Abs(j - Position);                        
+                    }
+                    else
+                    {
+                        for (int j = Crabs[i]; j < Position; j++)                        
+                            Distance += Math.Abs(j - Position);
+                    }
+                }
+                if (Distance < Res || Position == 1)
+                    Res = Distance;
+                Position++;
+            }
+            Console.WriteLine(Res);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using AdventOfCode._2021;
 using AdventOfCode.Helpers;
 HelperClass helper = new HelperClass();
-Day6 Day = new Day6();
+Day7 Day = new Day7();
 
 Console.WriteLine("Part1: ");
 Day.FirstStar();
